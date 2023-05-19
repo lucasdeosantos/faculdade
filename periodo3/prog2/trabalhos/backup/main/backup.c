b0e7d8ee4ec204f78329a5256cec36be77783081
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2023-05-19 01:55:29 -0300

    corrigindo identaçao /prog2/trabalhos/backup

--- periodo3/prog2/trabalhos/backup/main/backup.c
+++ periodo3/prog2/trabalhos/backup/main/backup.c
@@ -13,73 +13,73 @@ void nomeArquivo(FILE *binario, char nome[]) {
 }
 
 void extraiArquivos(FILE* binario) {
-    unsigned int numRegistros, tamRegistro;
+	unsigned int numRegistros, tamRegistro;
 	char nome[BUFFER_SIZE];
 
-    fread(&numRegistros, sizeof(unsigned int), 1, binario);
+	fread(&numRegistros, sizeof(unsigned int), 1, binario);
 
-    for(unsigned int i = 0; i < numRegistros; i++) {
-        fread(&tamRegistro, sizeof(unsigned int), 1, binario);
+	for(unsigned int i = 0; i < numRegistros; i++) {
+		fread(&tamRegistro, sizeof(unsigned int), 1, binario);
 		nomeArquivo(binario, nome);
 
-        char *conteudo = (char*) malloc(tamRegistro - strlen(nome) - 1);
-        fread(conteudo, 1, tamRegistro - strlen(nome) - 1, binario);
+		char *conteudo = (char*) malloc(tamRegistro - strlen(nome) - 1);
+		fread(conteudo, 1, tamRegistro - strlen(nome) - 1, binario);
 
-        FILE *arquivo;
+		FILE *arquivo;
 
 		arquivo = fopen(nome, "wb");
-        if(!arquivo) {
+		if(!arquivo) {
 			perror("Erro ao abrir arquivo");
-            free(conteudo);
-            exit(EXIT_FAILURE);
-        }
+			free(conteudo);
+			exit(EXIT_FAILURE);
+		}
 
-        fwrite(conteudo, 1, tamRegistro - strlen(nome) - 1, arquivo);
+		fwrite(conteudo, 1, tamRegistro - strlen(nome) - 1, arquivo);
 
-        free(conteudo);
-        fclose(arquivo);
-    }
+		free(conteudo);
+		fclose(arquivo);
+	}
 }
 
 void imprimeArquivos(FILE *binario) {
-    unsigned int numRegistros, tamRegistro;
+	unsigned int numRegistros, tamRegistro;
 	char nome[BUFFER_SIZE];
 
-    fread(&numRegistros, sizeof(unsigned int), 1, binario);
+	fread(&numRegistros, sizeof(unsigned int), 1, binario);
 
-    for(unsigned int i = 0; i < numRegistros; i++) {
-        fread(&tamRegistro, sizeof(unsigned int), 1, binario);
+	for(unsigned int i = 0; i < numRegistros; i++) {
+		fread(&tamRegistro, sizeof(unsigned int), 1, binario);
 		nomeArquivo(binario, nome);
-        printf("%s, %u bytes\n", nome, (unsigned int)(tamRegistro - strlen(nome) - 1));
-        fseek(binario, tamRegistro - strlen(nome) - 1, SEEK_CUR);
-    }
+		printf("%s, %u bytes\n", nome, (unsigned int)(tamRegistro - strlen(nome) - 1));
+		fseek(binario, tamRegistro - strlen(nome) - 1, SEEK_CUR);
+	}
 }
 
 int main(int argc, char **argv) {
-    int opt, opt_t = 0;
+	int opt, opt_t = 0;
 
-    while((opt = getopt(argc, argv, "t")) != -1) {
-        switch (opt) {
-            case 't':
-                opt_t = 1;
-                break;
+	while((opt = getopt(argc, argv, "t")) != -1) {
+		switch (opt) {
+			case 't':
+				opt_t = 1;
+				break;
 
-            default:
+			default:
 				fprintf(stderr, "Uso: ./backup [-t]\n");
 				exit(EXIT_FAILURE);
-        }
-    }
+		}
+	}
 
-    FILE *binario = fopen("bkp.bin", "rb");
+	FILE *binario = fopen("bkp.bin", "rb");
 
-    if(!binario) {
+	if(!binario) {
 		perror("Erro ao abrir arquivo");
-        exit(EXIT_FAILURE);
-    }
+		exit(EXIT_FAILURE);
+	}
 
-    if(opt_t)
-        imprimeArquivos(binario);
-    else
+	if(opt_t)
+		imprimeArquivos(binario);
+	else
 		extraiArquivos(binario);
 
 	fclose(binario);
