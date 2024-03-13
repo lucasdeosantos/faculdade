798b875d4cd37008d6f828d437648b050c4acf48
Lucas Santos <lucasdeoliveirsantos@gmail.com>
2024-03-12 21:49:18 -0300

    corringo identacao rainhas

--- periodo2/alg2/rainhas/main.c
+++ periodo2/alg2/rainhas/main.c
@@ -3,76 +3,76 @@
 
 void imprimeTabuleiro(int v[], int n){
 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++)
-			if(v[i] == j)
-				printf("1 ");
-			else
-				printf("0 ");
-		printf("\n");
-	}
-	printf("\n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)
+            if(v[i] == j)
+                printf("1 ");
+            else
+                printf("0 ");
+        printf("\n");
+    }
+    printf("\n");
 
-	return;
+    return;
 }
 
 void rainhasRecursivo(int v[], int n, int r){
 
-	if(r == n){
-		imprimeTabuleiro(v, n);
-		return;
-	}
-	for(int i = 0; i < n; i++){
-		int bool = 1;
-		for(int j = 0; j < r; j++){
-			if(v[j] == i || v[j] == i+r-j || v[j] == i-r+j)
-				bool = 0;
-		}
-		if(bool){
-			v[r] = i;
-			rainhasRecursivo(v, n, r+1);
-		}
-	}
-	return;
+    if(r == n){
+        imprimeTabuleiro(v, n);
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        int bool = 1;
+        for(int j = 0; j < r; j++){
+            if(v[j] == i || v[j] == i+r-j || v[j] == i-r+j)
+                bool = 0;
+        }
+        if(bool){
+            v[r] = i;
+            rainhasRecursivo(v, n, r+1);
+        }
+    }
+    return;
 }
 
 void rainhas(int v[], int n, int r){
-	t_pilha *p;
-	
-	p = cria_pilha(n + 1);
+    t_pilha *p;
 
-	empilha(r, p);
+    p = cria_pilha(n + 1);
 
-	while(!pilha_vazia(p)){
+    empilha(r, p);
 
-		desempilha(&r, p);
+    while(!pilha_vazia(p)){
 
-		if(r == n)
-			imprimeTabuleiro(v, n);
+        desempilha(&r, p);
 
-		for(int i = 0; i < n; i++){
-			int bool = 1;
-			for(int j = 0; j < r; j++){
-				if(v[j] == 1 || v[j] == i+r-j || v[j] == i-r+j)
-					bool = 0;
-			}
-			if(bool){
-				v[r] = i;
-				empilha(r+1, p);
-				break;
-			}
-		}
-	}
-	destroi_pilha(p);
+        if(r == n)
+            imprimeTabuleiro(v, n);
+
+        for(int i = 0; i < n; i++){
+            int bool = 1;
+            for(int j = 0; j < r; j++){
+                if(v[j] == 1 || v[j] == i+r-j || v[j] == i-r+j)
+                    bool = 0;
+            }
+            if(bool){
+                v[r] = i;
+                empilha(r+1, p);
+                break;
+            }
+        }
+    }
+    destroi_pilha(p);
 }
 
 int main(){
-	int n;
-	scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-	int v[n];
+    int v[n];
     // rainhas(v, n, 0);
-	rainhasRecursivo(v, n, 0);
+    rainhasRecursivo(v, n, 0);
 
-	return 0;
+    return 0;
 }
