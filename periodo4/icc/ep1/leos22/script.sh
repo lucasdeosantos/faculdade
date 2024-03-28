f0189b5c1522edfe46750fbc3600a5fabaed7b49
Lucas Santos <lucasdeoliveirsantos@gmail.com>
2024-03-28 17:57:04 -0300

    adding a script to test all ls inputs

--- periodo4/icc/ep1/leos22/script.sh
+++ periodo4/icc/ep1/leos22/script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Execute the program perfSL through LIKWID
+# Check if perfSL executable exists, if not, run make
+if [ ! -x ./perfSL ]; then
+    make
+fi
+
+# Execute the program perfSL through LIKWID, and show only DP MFLOP/s
 likwid-perfctr -C 0-3 -g FLOPS_DP ./perfSL 2>/dev/null | grep "|      DP MFLOP/s      |"
