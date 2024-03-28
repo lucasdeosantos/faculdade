a9e2bae3b18f5b75cb32ecd499698f55d2462700
Lucas Santos <lucasdeoliveirsantos@gmail.com>
2024-03-28 14:07:36 -0300

    updating script.sh so it only shows the MPFLOPS

--- periodo4/icc/ep1/leos22/script.sh
+++ periodo4/icc/ep1/leos22/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-likwid-perfctr -C 0-3 -g FLOPS_DP - m ./perfSL 2>/dev/null | grep "DP MFLOP/s"
+# Execute the program perfSL through LIKWID
+likwid-perfctr -C 0-3 -g FLOPS_DP ./perfSL 2>/dev/null | grep "|      DP MFLOP/s      |"
