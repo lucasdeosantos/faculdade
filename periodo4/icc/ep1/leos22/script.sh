641237e08ef139ee0f663a23c09cd4ccaf15ff20
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-04-03 14:27:52 -0300

    Update script.sh

--- periodo4/icc/ep1/leos22/script.sh
+++ periodo4/icc/ep1/leos22/script.sh
@@ -6,5 +6,4 @@ if [ ! -x ./perfSL ]; then
 fi
 
 # Execute the program perfSL through LIKWID, and show only DP MFLOP/s
-likwid-perfctr -C 0-3 -g FLOPS_DP ./perfSL 2>/dev/null 
-# | grep "|      DP MFLOP/s      |"
+likwid-perfctr -C 1 -g FLOPS_DP -m ./perfSL | grep 'DP' | grep -v 'AVX'
