d3825e5a0687bd34bf97f1ebd50f9193ef1ea704
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-04-04 11:07:54 -0300

    Update gs.h

--- periodo4/icc/ep1/leos22/gs.h
+++ periodo4/icc/ep1/leos22/gs.h
@@ -11,12 +11,16 @@
 // Max number of iterations.
 #define MAXIT 50
 
-// Performs the Gauss-Seidel iterative method to solve a linear system, with a
-// specified tolerance for convergence. Returns the number of iterations performed.
+/*
+* The following methods perform the Gauss-Seidel iterative method to solve a linear system,
+* and returns the number of iterations performed.
+* x is the solution array.
+* tol is a specified tolerance for convergence.
+*/
+
 int gaussSeidel(LS_t *ls, real_t *x, real_t tol);
 
-// Performs the Gauss-Seidel iterative method optimized for tridiagonal linear systems, with a
-// specified tolerance for convergence. Returns the number of iterations performed.
+// Method optimized for tridiagonal linear systems.
 int gaussSeidelTridiagonal(LS_t *ls, real_t *x, real_t tol);
 
 #endif // GS_H
