a6c05f99b949c7a9bc90a5e224164b1e909fc0a3
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-04-04 15:47:06 -0300

    Update script.sh

--- periodo4/icc/ep1/leos22/script.sh
+++ periodo4/icc/ep1/leos22/script.sh
@@ -5,5 +5,8 @@ if [ ! -x ./perfSL ]; then
     make
 fi
 
+# Export the likwid library path
+export LD_LIBRARY_PATH=/home/soft/likwid/lib:$(LD_LIBRARY_PATH)
+
 # Execute the program perfSL through LIKWID, and show only DP MFLOP/s
 likwid-perfctr -C 8 -g FLOPS_DP -m ./perfSL | grep 'DP' | grep -v 'AVX'
