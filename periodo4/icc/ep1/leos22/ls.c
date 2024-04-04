1c69f866923f86fe4f6e6adb5ca0ecf718ee19de
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-04-04 14:28:40 -0300

    Update ls.c

--- periodo4/icc/ep1/leos22/ls.c
+++ periodo4/icc/ep1/leos22/ls.c
@@ -8,13 +8,13 @@
 #include <string.h>
 #include "ls.h"
 
-LS_t *allocLS(int n) {
+LS_t *allocLS(int_t n) {
     // Allocate memory for the LS_t structure.
     LS_t *ls = (LS_t*) malloc(sizeof(LS_t));
 
     // Allocate memory for the coefficient matrix (2D array).
     ls->A = (real_t**) malloc(n * sizeof(real_t*));
-    for (int i = 0; i < n; ++i)
+    for (int_t i = 0; i < n; ++i)
         ls->A[i] = (real_t*) malloc(n * sizeof(real_t));
 
     // Allocate memory for the constants array.
@@ -28,7 +28,7 @@ LS_t *allocLS(int n) {
 
 void freeLS(LS_t *ls) {
     // Free memory for the coefficient matrix.
-    for (int i = 0; i < ls->n; ++i)
+    for (int_t i = 0; i < ls->n; ++i)
         free(ls->A[i]);
     free(ls->A);
 
@@ -40,9 +40,9 @@ void freeLS(LS_t *ls) {
 }
 
 void readLS(LS_t *ls) {
-    for (int i = 0; i < ls->n; ++i) {
+    for (int_t i = 0; i < ls->n; ++i) {
         // Read coefficients of the equation.
-        for (int j = 0; j < ls->n; ++j)
+        for (int_t j = 0; j < ls->n; ++j)
             scanf("%lf", &ls->A[i][j]);
 
         // Read the constants array.
@@ -51,13 +51,13 @@ void readLS(LS_t *ls) {
 }
 
 void diagonalLS(LS_t *ls, real_t *d, int i, int j) {
-    for (int k = 0; k + i < ls->n && k + j < ls->n; ++k)
+    for (int_t k = 0; k + i < ls->n && k + j < ls->n; ++k)
         d[k] = ls->A[k + i][k + j];
 }
 
 void copyLS(LS_t *dst, LS_t *src) {
     // Copy coefficients matrix.
-    for (int i = 0; i < src->n; ++i)
+    for (int_t i = 0; i < src->n; ++i)
         memcpy(dst->A[i], src->A[i], src->n * sizeof(real_t));
 
     // Copy constants array.
@@ -68,8 +68,8 @@ void copyLS(LS_t *dst, LS_t *src) {
 }
 
 void printLS(LS_t *ls) {
-     for (int i = 0; i < ls->n; ++i) {
-        for (int j = 0; j < ls->n; ++j)
+     for (int_t i = 0; i < ls->n; ++i) {
+        for (int_t j = 0; j < ls->n; ++j)
             printf(".12%lf ", ls->A[i][j]);
 
         printf("%.12lf\n", ls->b[i]);
@@ -79,13 +79,13 @@ void printLS(LS_t *ls) {
 // Function to compute the residue array of a linear system.
 // R = AX - B
 void residueLS(LS_t *ls, real_t *x, real_t *r) {
-    for (int i = 0; i < ls->n; ++i) {
+    for (int_t i = 0; i < ls->n; ++i) {
         r[i] = 0.0;
-        for (int j = 0; j < ls->n; ++j)
+        for (int_t j = 0; j < ls->n; ++j)
             r[i] += ls->A[i][j] * x[j];
     }
 
-    for (int i = 0; i < ls->n; ++i)
+    for (int_t i = 0; i < ls->n; ++i)
         r[i] -= ls->b[i];
 }
 
@@ -94,12 +94,12 @@ void printSolution(LS_t *ls, real_t *x, real_t *r) {
     residueLS(ls, x, r);
 
     // Print the solution array.
-    for (int i = 0; i < ls->n; ++i)
+    for (int_t i = 0; i < ls->n; ++i)
         printf("%.12f ", x[i]);
     printf("\n");
 
     // Print the residue array.
-    for (int i = 0; i < ls->n; ++i)
+    for (int_t i = 0; i < ls->n; ++i)
         printf("%.12f ", r[i]);
     printf("\n");
 }
