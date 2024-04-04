47a00af2cf9a442f2353f8cd2f0321e79a1e3ce5
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-04-04 14:31:36 -0300

    Update ge.c

--- periodo4/icc/ep1/leos22/ge.c
+++ periodo4/icc/ep1/leos22/ge.c
@@ -8,11 +8,11 @@
 #include "ge.h"
 
 // Function to find the row with the maximum value in a given column.
-int findMax(LS_t *ls, int i) {
+int findMax(LS_t *ls, int_t i) {
     real_t max = ls->A[i][i];
     int index = i;
 
-    for (int j = i + 1; j < ls->n; ++j)
+    for (int_t j = i + 1; j < ls->n; ++j)
         if (ls->A[j][i] > max) {
             max = ls->A[j][i];
             index = j;
@@ -22,7 +22,7 @@ int findMax(LS_t *ls, int i) {
 }
 
 // Function to swap two rows in the coefficient matrix and the constants array.
-void lineSwap(LS_t *ls, unsigned int i, int iPivo) {
+void lineSwap(LS_t *ls, int_t i, int_t iPivo) {
     real_t *temp = (real_t*) malloc(ls->n * sizeof(real_t));
 
     // Swap rows in the coefficient matrix.
@@ -40,9 +40,9 @@ void lineSwap(LS_t *ls, unsigned int i, int iPivo) {
 
 // Function to perform back substitution to solve the system of equations.
 void retrosSusbs(LS_t *ls, real_t *x) {
-    for (int i = ls->n - 1; i >= 0; --i) {
+    for (int_t i = ls->n - 1; i >= 0; --i) {
         x[i] = ls->b[i];
-        for (int j = i + 1; j < ls->n; ++j)
+        for (int_t j = i + 1; j < ls->n; ++j)
             x[i] -= ls->A[i][j] * x[j];
 
         x[i] /= ls->A[i][i];
@@ -50,17 +50,17 @@ void retrosSusbs(LS_t *ls, real_t *x) {
 }
 
 void gaussElimination(LS_t *ls, real_t *x) {
-    for (int i = 0; i < ls->n; ++i) {
+    for (int_t i = 0; i < ls->n; ++i) {
         // Find the row with the maximum value in the current column.
-        int iPivo = findMax(ls, i);
+        int_t iPivo = findMax(ls, i);
         if (i != iPivo)
            lineSwap(ls, i, iPivo);
 
         // Perform elimination.
-        for (int k = i + 1; k < ls->n; ++k) {
+        for (int_t k = i + 1; k < ls->n; ++k) {
             real_t m = ls->A[k][i] / ls->A[i][i];
             ls->A[k][i] = 0.0;
-            for (int j = i + 1; j < ls->n; ++j)
+            for (int_t j = i + 1; j < ls->n; ++j)
                 ls->A[k][j] -= ls->A[i][j] * m;
             ls->b[k] -= ls->b[i] * m;
         }
@@ -81,7 +81,7 @@ void gaussTridiagonalElimination(LS_t *ls, real_t *x) {
     diagonalLS(ls, c, 0, 1);
 
     // Forward elimination.
-    for (int i = 0; i < ls->n - 1; ++i) {
+    for (int_t i = 0; i < ls->n - 1; ++i) {
         real_t m = a[i] / d[i];
         a[i] = 0.0;
         d[i + 1] -= c[i] * m;
@@ -90,7 +90,7 @@ void gaussTridiagonalElimination(LS_t *ls, real_t *x) {
 
     // Back substitution.
     x[ls->n - 1] = ls->b[ls->n - 1] / d[ls->n - 1];
-    for (int i = ls->n - 2; i >= 0; --i)
+    for (int_t i = ls->n - 2; i >= 0; --i)
         x[i] = (ls->b[i] - c[i] * x[i + 1]) / d[i];
 
     // Free allocated memory.
