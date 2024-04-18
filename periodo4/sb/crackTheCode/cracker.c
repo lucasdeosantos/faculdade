c3825e1e2087f66abb4a55b7c07ab14a59338bd8
Lucas Santos <lucasdeoliveirsantos@gmail.com>
2024-04-18 12:52:19 -0300

    tentando fazer trabalho de sb

--- periodo4/sb/crackTheCode/cracker.c
+++ periodo4/sb/crackTheCode/cracker.c
@@ -2,56 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFF_SIZE 1 //DEFINA O TAMANHO DO BUFFER AQUI
+#define BUFF_SIZE 10 //DEFINA O TAMANHO DO BUFFER AQUI
 
 void execute() {
-  system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
-  system("./ctc");
-  system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
+    system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
+    system("./ctc");
+    system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
 }
 
 void debug() {
-  system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
-  system("gdb ./ctc");
-  system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
+    system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
+    system("gdb ./ctc");
+    system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
 }
 
 void crack() {
-  FILE *f = fopen("./teste.in", "w+");
-  char buff[BUFF_SIZE];
-  //IMPLEMENTE A ENTRADA PARA ALCANÇAR OS OBJETIVOS DO TRABALHO AQUI (USAR A ENTRADA PADRÃO)
-  fwrite(buff, 1, BUFF_SIZE, f);  
-  fclose(f);
+    FILE *f = fopen("./teste.in", "w+");
+    char buff[BUFF_SIZE];
+    char exploit[BUFF_SIZE + 1000000];
+    strcpy(buff, "SB\nSB\n");
+    strcpy(exploit, buff);
+    //IMPLEMENTE A ENTRADA PARA ALCANÇAR OS OBJETIVOS DO TRABALHO AQUI (USAR A ENTRADA PADRÃO)
+    fwrite(buff, 1, BUFF_SIZE, f);  
+    fclose(f);
 
-  system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
-  system("./ctc < teste.in");
-  system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
+    system("sudo echo 0 > /proc/sys/kernel/randomize_va_space");
+    system("./ctc < teste.in");
+    system("sudo echo 2 > /proc/sys/kernel/randomize_va_space");
 }
 
 int main(int argc, char **argv) {
-  
-  if (argc == 2){ 	
-    if (!strcmp(argv[1], "1")){
-      execute(); 
-      return 0;
-    } 
-    if (!strcmp(argv[1], "2")){
-      debug();
-      return 0;
+
+    if (argc == 2){ 	
+        if (!strcmp(argv[1], "1")){
+            execute(); 
+            return 0;
+        } 
+        if (!strcmp(argv[1], "2")){
+            debug();
+            return 0;
+        }
+        if (!strcmp(argv[1], "3")){
+            crack();
+            return 0;
+        }
     }
-    if (!strcmp(argv[1], "3")){
-      crack();
-      return 0;
-    }
-  }
-  
-  printf("=========== HELP ===========\n");
-  printf("EXECUTE AS SUPER USER!!\n");
-  printf("./cracker [1|2|3]\n");
-  printf("1 -> Execute\n");
-  printf("2 -> Debug\n");
-  printf("3 -> Crack\n");
-  printf("============================\n");
-  return 1;
-  
+
+    printf("=========== HELP ===========\n");
+    printf("EXECUTE AS SUPER USER!!\n");
+    printf("./cracker [1|2|3]\n");
+    printf("1 -> Execute\n");
+    printf("2 -> Debug\n");
+    printf("3 -> Crack\n");
+    printf("============================\n");
+    return 1;
 }
