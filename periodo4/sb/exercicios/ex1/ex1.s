54541a9ed14a23729c4798353eab330b93cc216f
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-07-10 14:02:08 -0300

    adicionando exercicio de icc

--- periodo4/sb/exercicios/ex1/ex1.s
+++
@@ -1,6 +0,0 @@
-.section .text
-.global _start
-_start:
-    movq $0, %rdi
-    movq $60, %rax
-    syscall
--- periodo4/sb/exercicios/ex7/ex7.c
+++
@@ -1,9 +0,0 @@
-int area(int r) {
-    return r*r * 3;
-}
-
-int main() {
-    int r = 3;
-
-    return area(r);
-}
