45c3c3c177d98b9287a2345b50aebde3f0d5e248
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-07-24 17:41:20 -0300

    Update lista4.hs

--- periodo4/paradigmas/haskell/lista4/lista4.hs
+++ periodo4/paradigmas/haskell/lista4/lista4.hs
@@ -16,11 +16,11 @@ type Grupo = [Pesquisador]
 base :: Int -> Pesquisador
 base x
     | x == 1 = ("joao", "mestre", 'm')
-    |x == 2 = ("jonas", "doutor", 'm')
-    |x == 3 = ("joice", "mestre", 'f')
-    |x == 4 = ("janete", "doutor", 'f')
-    |x == 5 = ("jocileide", "doutor", 'f')
-    |otherwise = ("ninguem", "", 'x')
+    | x == 2 = ("jonas", "doutor", 'm')
+    | x == 3 = ("joice", "mestre", 'f')
+    | x == 4 = ("janete", "doutor", 'f')
+    | x == 5 = ("jocileide", "doutor", 'f')
+    | otherwise = ("ninguem", "", 'x')
 
 pesquisadoresBase :: Int -> Grupo
 pesquisadoresBase 0 = []
