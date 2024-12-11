b98f3b71a95d24d38699f39fd56b7713d1289649
Lucas Emanuel de Oliveira Santos <lucasdeoliveirsantos@gmail.com>
2024-12-11 18:54:52 -0300

    adicionando trab3 de paralela

---
+++ periodo5/paralela/trab3/verifica_particoes.h
@@ -0,0 +1,10 @@
+//
+// By Lucas Emanuel de Oliveira Santos
+//
+#ifndef VERIFICA_PARTICOES_H
+#define VERIFICA_PARTICOES_H
+
+void verifica_particoes(long long *Input, int n, long long *P, int np, long long *Output, int *nO);
+
+#endif // VERIFICA_PARTICOES_H
+
